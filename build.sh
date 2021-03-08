c574aa7e429c6d66d97f7a9a9586ccc9ce4283c8
Ibrahim Umar <ibrahim.umar@hi.no>
2021-03-08 22:46:01 +0100

    Update build.sh to push images to Docker Hub

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-# Make the base image first as the requirements for the following images
-docker build -f Dockerfile.crimac-tx2-base -t crimac-tx2-base .
+# Create and push the base image first as the requirements for the following images
+docker build -f Dockerfile.crimac-tx2-base -t docker.io/crimac/jetson-tx2-base .
+docker push docker.io/crimac/jetson-tx2-base
 
-# Create both pytorch and preprocessing images
-docker build -f Dockerfile.crimac-tx2-pytorch -t crimac-tx2-pytorch .
-docker build -f Dockerfile.crimac-tx2-preprocess -t crimac-tx2-preprocess .
+# Create and pus both preprocessing and pytorch images
+docker build -f Dockerfile.crimac-tx2-preprocess -t docker.io/crimac/jetson-tx2-preprocess .
+docker push docker.io/crimac/jetson-tx2-base
 
-# TODO: Push images to docker hub
+docker build -f Dockerfile.crimac-tx2-pytorch -t docker.io/crimac/jetson-tx2-pytorch .
+docker push docker.io/crimac/jetson-tx2-pytorch
