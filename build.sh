f9903650d70ea616005760cf666748c1def53f7a
Ibrahim Umar <ibrahim.umar@hi.no>
2021-03-09 09:34:52 +0100

    Fix type in build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ docker push docker.io/crimac/jetson-tx2-base
 
 # Create and pus both preprocessing and pytorch images
 docker build -f Dockerfile.crimac-tx2-preprocess -t docker.io/crimac/jetson-tx2-preprocess .
-docker push docker.io/crimac/jetson-tx2-base
+docker push docker.io/crimac/jetson-tx2-preprocess
 
 docker build -f Dockerfile.crimac-tx2-pytorch -t docker.io/crimac/jetson-tx2-pytorch .
 docker push docker.io/crimac/jetson-tx2-pytorch
